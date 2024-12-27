996f81936902b59937d30e7985123f682f8aa81c
baohuy2209 <huynguyen@002311@gmail.com>
2024-12-27 20:38:45 +0700

    Update

--- news-blog-app/src/components/Calendar.jsx
+++ news-blog-app/src/components/Calendar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Calendar.css";
 const Calendar = () => {
-  return <div className="calendar">Calendar</div>;
+  return <div className="calendar">Hello Calendar</div>;
 };
 
 export default Calendar;
